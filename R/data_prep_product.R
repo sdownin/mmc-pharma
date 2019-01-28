5f177e4cb6974092eadd57db454a59724203a1e8
Stephen Downing <stephendowning2008@gmail.com>
2019-01-27 20:42:03 -0600

    updates product data prep script

--- R/data_prep_product.R
+++ R/data_prep_product.R
@@ -15,6 +15,7 @@
 
 library(stringr)
 library(readxl)
+library(dbplyr)
 
 proj_dir <- "C:/Users/T430/Google Drive/PhD/Research/MMC/pharma_encounters/mmc-pharma"
 medtrack_dir <- file.path(proj_dir,"medtrack_data")
@@ -25,11 +26,18 @@ setwd(proj_dir)
 ## Excel files
 files <- dir(medtrack_product_dir, pattern = "\\.xlsx{0,1}$")
 
-## lines to skip for Medtrack header atop data table
-skip.lines <- 11
+# ## lines to skip for Medtrack header atop data table
+# skip.lines <- 1
 
-## init combined dataframe
-dfall <- data.frame()
+## get sheets
+sheets <- excel_sheets(file.path(medtrack_product_dir, file))
+sheets <- sheets[!grepl("^Sheet",sheets,ignore.case = T)]
+
+## init list of combined dataframes
+l <- list()
+for (sheet in sheets) {
+  l[[sheet]] <- data.frame()
+}
 
 ## load files loop
 for (file in files) {
@@ -40,18 +48,45 @@ for (file in files) {
   parts <- str_split(file,"[_]")[[1]]
   category <- str_split(parts[length(parts)],"[.]")[[1]][1]
   
-  ## load data
-  file_full_path <- file.path(medtrack_product_dir, file)
-  df <- read_excel(file_full_path, sheet = 1, na=NA, skip = skip.lines)
+  ## loops sheets in workbook
+  for (sheet in sheets) {
+    cat(sprintf("  sheet %s\n", sheet))
+    
+    ## load data
+    file_full_path <- file.path(medtrack_product_dir, file)
+    
+    ## deleted header from Product Synopsis sheet but not from others
+    ## skip 11 lines of header material in other sheets
+    if (sheet == "Product Synopsis") {
+      df <- read_excel(file_full_path, sheet = sheet, na="--")
+    } else {
+      df <- read_excel(file_full_path, sheet = sheet, na="--", skip = 11)
+    }
+    
+    ## clean column names
+    names(df) <- str_to_lower(str_replace_all(names(df),"[\\s\\/]+","_"))
+    
+    ## add category
+    df$therapeutic_class <- category
+    
+    ## append rows to combined dataframe
+    l[[sheet]] <- rbind(l[[sheet]], df)
+  }
   
-  ## fix column names
-  names(df) <- str_replace_all(names(df),"\\s+","_")
-  
-  ## add category
-  df$therapeutic_class <- category
-  
-  ## append rows to combined dataframe
-  dfall <- rbind(dfall, df)
 }
 
+print(summary(l))
+
+out_file <- file.path(medtrack_dir, "products_list.rds")
+saveRDS(l, file = out_file)
+
 ## end
+
+
+
+
+##------------------------ Descriptives ------------------
+prcnt <- plyr::count(l$`Product Synopsis`$highest_phase_of_development)
+prcnt <- prcnt[c(9,8,5,6,7,4,1,3,2),]
+prcnt$pct <- round(100 * prcnt$freq / sum(prcnt$freq), 1)
+print(prcnt)
