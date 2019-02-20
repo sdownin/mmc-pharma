60e2ae7a8a4ef851e6415a48f191891942000587
Stephen Downing <stephendowning2008@gmail.com>
2019-02-19 18:30:49 -0600

    updates to firm and product sales analysis (in progress)

--- R/data_prep_sales_firm.R
+++ R/data_prep_sales_firm.R
@@ -57,36 +57,30 @@ id.vars <- c("Product Name", "Companies", "Currency", "Region")
 measure.vars <- names(df)[ ! names(df) %in% id.vars ]
 dfl <- as_tibble(reshape2::melt(df, id.vars, measure.vars, variable.name="year", value.name="sales"))
 
-##
-exclude.regions <- c("Total Global Sales", "Worldwide","Rest Of World")
-
-
 ## Check regions frequency
 region.summary <- dfl %>%
-  filter(!is.na(Region) & (! Region %in% exclude.regions )) %>%
+  filter(!is.na(Region) & (! Region %in% c("Total Global Sales", "Worldwide") )) %>%
   group_by(Region, Currency) %>%
   tally(sort = T) %>%
   ungroup() %>%
   arrange(desc(n))
 print(region.summary)
-# ## same result using count() instead of group_by(),tally(),ungroup()
-# region.summary2 <- dfl %>%
-#   filter(!is.na(Region)) %>%
-#   count(Region, Currency) %>%
-#   arrange(desc(n))
+
 
 ## Firms by regional sphere_group of influence (coarse separation by continent)
 firms.us <- unique(
   c("Johnson & Johnson",'Pfizer','Amgen','Biogen','Eli Lilly','AbbVie',
     'Merck','Abbott','Gilead Sciences','Bristol-Myers Squibb')
-  )
+)
 firms.eu <- unique(
   c('Novartis','GlaxoSmithKline','AstraZeneca','Roche',
     'Sanofi','Bayer','Novo Nordisk')
-  )
+)
+firms <- c(firms.us, firms.eu)
 
 ## flag is single company product sales (not multiple) 
 dfl$is_single <- as.integer(sapply(dfl$Companies, function(x) length(strsplit(x,"[|]")[[1]])==1 ))
+
 
 ## region subnames
 r.us <- "USA"
@@ -96,12 +90,119 @@ r.jp <- "Japan"
 dfl$is_us <- as.integer(grepl(r.us, dfl$Region))
 dfl$is_eu <- as.integer(grepl(r.eu, dfl$Region))
 dfl$is_jp <- as.integer(grepl(r.jp, dfl$Region))
+dfl$region_sales <- apply(dfl[,c('is_us','is_eu','is_jp')], 1, function(x) {
+  if(x[1]==1 & x[2]==0 & x[3]==0) return("USA")
+  if(x[1]==0 & x[2]==1 & x[3]==0) return("Europe")
+  if(x[1]==0 & x[2]==0 & x[3]==1) return("Japan")
+  if(sum(x) > 1 ) return("Multiple")
+  if(x[1]==0 & x[2]==0 & x[3]==0) return("Other")
+  return("Other")
+})
+
+
+# for (firm in firms) {
+#   cat(sprintf(" applying firm %s\n",firm))
+#   dfl[,firm] <- apply(dfl[,"Companies"], 1, function(product_comanies){
+#     ifelse(grepl(firm, product_comanies), 1, 0)
+#   })
+# }
+
+dfl$firm_name <- apply(dfl[,"Companies"], 1, function(product_comanies){
+    for (firm in firms) {
+      if (grepl(firm, product_comanies)) {
+        return(firm)
+      }
+    }
+    return("Other")
+  })
+
+dfl$sphere_group <- sapply(dfl$firm_name, function(firm) {
+  ifelse(firm %in% firms.us,'American',ifelse(firm %in% firms.eu,'European','Other'))
+})
+
+
+## sum products by sphere_group of influence group
+grp.sales <- dfl %>%
+  filter(any(sapply(firms, function(firm)grepl(firm,Companies)))) %>%
+  group_by(firm_name, sphere_group, region_sales, year) %>%
+  summarise(sales_total = sum(sales, na.rm=T))
+## fix data types
+grp.sales$year <- as.integer(as.character(grp.sales$year))
+# grp.sales$`Product Name` <- as.factor(grp.sales$`Product Name`)
+grp.sales$sp <- as.factor(grp.sales$firm_name)
+grp.sales$sphere_group <- as.factor(grp.sales$sphere_group)
+grp.sales$region_sales <- as.factor(grp.sales$region_sales)
+grp.sales$date <- as.Date(sapply(grp.sales$year, function(t)sprintf('%s-01-01',t)))
+View(grp.sales)
+
+## plotting
+library(ggplot2)
+
+## data subset for plot
+plt.idx <- which(
+  grp.sales$year > 2002 & grp.sales$year < 2018 & 
+  ! grp.sales$firm_name %in% c("Other")  &
+  ! grp.sales$region_sales %in% c("Other","Multiple","Japan")  #&
+  #grp.sales$firm_name %in% firms.eu
+    # grp.sales$firm_name %in% c('Gilead Sciences') & 
+    # grp.sales$`Product Name` %in% c('Harvoni','Sovaldi')
+)
+df.plot <- grp.sales[plt.idx, ]
+
+## firm subset
+
+
+##--------------
+## Plotting
+##-------------
+## annotations
+a.y <- max(df.plot$sales_total/1000) * .9
+##plot figure
+rev.plot1 <- ggplot(aes(x=date, y=sales_total/1000, colour=firm_name), data=df.plot) + 
+  geom_line() + #geom_point() + 
+  facet_grid(region_sales ~ sphere_group) +
+  geom_vline(xintercept = as.Date("2013-01-01"), lty=2, lwd=1.05, col='gray') + 
+  geom_vline(xintercept = as.Date("2010-03-01"), lty=3, lwd=1.05, col='gray') + 
+  ylab("Total Sales (US$ Bn.)") + xlab("Year") + 
+  labs(colour = "Firm") +
+  ggtitle("Impact of the PPACA on Pharmaceutical Sales By Region") + 
+  # annotate("text", x=as.Date("2011-11-20"), y=a.y, label="Individual\nMandate", size=4, col='darkgray') +
+  # annotate("text", x=as.Date("2009-05-20"), y=a.y, label="PPACA\nSigned", size=4, col='darkgray') +
+  # scale_color_manual(values=c('black', gray.colors(1,.5,.5))) + 
+  theme_bw() + theme(legend.position = 'right')
+rev.plot1
+## save plot
+rev.plot1.filename <- file.path("img","firm_sales_EuropeUSAOther_revenue_total_pharma_sales.png")
+ggsave(filename = rev.plot1.filename, plot = rev.plot1, 
+       width=12, height=12, units='in', dpi=300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## region subnames
+r.us <- "USA"
+r.eu <- "Europe"
+## add region column
+dfl$is_us <- as.integer(grepl(r.us, dfl$Region) & !grepl(r.eu, dfl$Region))
+dfl$is_eu <- as.integer(grepl(r.eu, dfl$Region) & !grepl(r.us, dfl$Region))
 ## regional subset
-df.us <- dfl[dfl$is_us==1 & dfl$is_eu==0 & dfl$is_jp==0, ]
-df.eu <- dfl[dfl$is_us==0 & dfl$is_eu==1 & dfl$is_jp==0, ]
-df.jp <- dfl[dfl$is_us==0 & dfl$is_eu==0 & dfl$is_jp==1, ]
-df.bo <- dfl[ (dfl$is_us==1 & dfl$is_eu==1) | (dfl$is_us==1 & dfl$is_jp==1) | (dfl$is_eu==1 & dfl$is_jp==1) , ] ## both
-df.ot <- dfl[dfl$is_us==0 & dfl$is_eu==0 & dfl$is_jp==0, ] ## other (neither)
+df.us <- dfl[dfl$is_us==1, ]
+df.eu <- dfl[dfl$is_eu==1, ]
 
 ## split product sales by party
 ## use SIMPLE assumption of equal revenue share
@@ -148,84 +249,15 @@ dfsep.eu$sphere_group <- apply(dfsep.eu[,"Companies"], 1, function(product_coman
     return("Other")
   })
 
-##------------------------------
-## Japan
-##------------------------------
-## single firm
-dfx <- as_tibble(df.jp)
-dfx2 <- dfx[dfx$is_single==0, ]
-dfxsep <- dfx[dfx$is_single==1, ]
-## check if need to split companies
-if (nrow(dfx2)>0) print("need to split some products among multiple companies")
-## SPLIT IF NECESSARY
-## rename
-dfsep.jp <- dfxsep
-## firm group index
-dfsep.jp$sphere_group <- apply(dfsep.jp[,"Companies"], 1, function(product_comanies){
-  x.eu <- sapply(firms.eu, function(firm)grepl(firm, product_comanies))
-  if (any(x.eu)) return("European")
-  x.us <- sapply(firms.us, function(firm)grepl(firm, product_comanies))
-  if (any(x.us)) return("American")
-  return("Other")
-})
-
-
-##------------------------------
-## Both
-##------------------------------
-## single firm
-dfx <- as_tibble(df.bo)
-dfx2 <- dfx[dfx$is_single==0, ]
-dfxsep <- dfx[dfx$is_single==1, ]
-## check if need to split companies
-if (nrow(dfx2)>0) print("need to split some products among multiple companies")
-## SPLIT IF NECESSARY
-## rename
-dfsep.bo <- dfxsep
-## firm group index
-dfsep.bo$sphere_group <- apply(dfsep.bo[,"Companies"], 1, function(product_comanies){
-  x.eu <- sapply(firms.eu, function(firm)grepl(firm, product_comanies))
-  if (any(x.eu)) return("European")
-  x.us <- sapply(firms.us, function(firm)grepl(firm, product_comanies))
-  if (any(x.us)) return("American")
-  return("Other")
-})
-
-##------------------------------
-## Other
-##------------------------------
-## single firm
-dfx <- as_tibble(df.ot)
-dfx2 <- dfx[dfx$is_single==0, ]
-dfxsep <- dfx[dfx$is_single==1, ]
-## check if need to split companies
-if (nrow(dfx2)>0) print("need to split some products among multiple companies")
-## SPLIT IF NECESSARY
-## rename
-dfsep.ot <- dfxsep
-## firm group index
-dfsep.ot$sphere_group <- apply(dfsep.ot[,"Companies"], 1, function(product_comanies){
-  x.eu <- sapply(firms.eu, function(firm)grepl(firm, product_comanies))
-  if (any(x.eu)) return("European")
-  x.us <- sapply(firms.us, function(firm)grepl(firm, product_comanies))
-  if (any(x.us)) return("American")
-  return("Other")
-})
-
-
 ##--------------------------
 ## Combined
 ##--------------------------
 dfsep.us$region_sales <- "USA"
 dfsep.eu$region_sales <- "Europe"
-dfsep.jp$region_sales <- "Japan"
-dfsep.bo$region_sales <- "Multiple"
-dfsep.ot$region_sales <- "Other"
-dfsep <- rbind(dfsep.us, dfsep.eu, dfsep.jp, dfsep.ot, dfsep.bo)
+dfsep <- rbind(dfsep.us, dfsep.eu)
 
 ## Exclude companies outside of two main sphere_group of influence groups (USA,Europe)
-# dfsep2 <- dfsep[dfsep$sphere_group != "Other", ]
-dfsep2 <- dfsep
+dfsep2 <- dfsep[dfsep$sphere_group != "Other", ]
 
 ## sum products by sphere_group of influence group
 grp.sales <- dfsep2 %>% # filter(!is.na(Region)) %>%
@@ -237,18 +269,13 @@ grp.sales$year <- as.integer(as.character(grp.sales$year))
 grp.sales$sphere_group <- as.factor(grp.sales$sphere_group)
 grp.sales$region_sales <- as.factor(grp.sales$region_sales)
 grp.sales$date <- as.Date(sapply(grp.sales$year, function(t)sprintf('%s-01-01',t)))
-# View(grp.sales)
+View(grp.sales)
 
 ## plotting
 library(ggplot2)
 
 ## data subset for plot
-plt.idx <- which(
-  grp.sales$year > 2002 & grp.sales$year < 2018 & 
-  grp.sales$region_sales %in% c('USA','Europe','Japan') & 
-  grp.sales$sphere_group %in% c('American','European')
-)
-df.plot <- grp.sales[plt.idx, ]
+df.plot <- grp.sales[grp.sales$year > 2002 & grp.sales$year < 2018, ]
 
 # ## annotations
 # a.y <- max(df.plot$sales_total) * .87
@@ -265,7 +292,7 @@ df.plot <- grp.sales[plt.idx, ]
 ## annotations
 a.y <- max(df.plot$sales_total/1000) * .9
 ##plot figure
-rev.plot1 <- ggplot(aes(x=date, y=sales_total/1000, colour=sphere_group, pch=sphere_group), data=df.plot[df.plot$region_sales!='Japan',]) + 
+rev.plot1 <- ggplot(aes(x=date, y=sales_total/1000, colour=sphere_group, pch=sphere_group), data=df.plot) + 
   geom_line() + geom_point() + 
   facet_grid(region_sales ~ .) +
   geom_vline(xintercept = as.Date("2013-01-01"), lty=2, lwd=1.05, col='gray') + 
@@ -275,7 +302,7 @@ rev.plot1 <- ggplot(aes(x=date, y=sales_total/1000, colour=sphere_group, pch=sph
   ggtitle("Healthcare Legislation Natural Experiment:\nImpact of the PPACA on Pharmaceutical Sales in USA vs Europe") + 
   annotate("text", x=as.Date("2011-11-20"), y=a.y, label="Individual\nMandate", size=4, col='darkgray') +
   annotate("text", x=as.Date("2009-05-20"), y=a.y, label="PPACA\nSigned", size=4, col='darkgray') +
-  scale_color_manual(values=c('black', gray.colors(1,.5,.5),'steelblue')) + 
+  scale_color_manual(values=c('black', gray.colors(1,.5,.5))) + 
   theme_bw() + theme(legend.position = 'right')
 rev.plot1
 ## save plot
@@ -323,10 +350,8 @@ pvalstr <- function(p, alpha=0.01, digits=3) {
 ## [sphere_group].[regional_sales]
 us.us <- df.plot$sales_total[df.plot$sphere_group=="American" & df.plot$region_sales=="USA"]
 us.eu <- df.plot$sales_total[df.plot$sphere_group=="American" & df.plot$region_sales=="Europe"]
-us.jp <- df.plot$sales_total[df.plot$sphere_group=="American" & df.plot$region_sales=="Japan"]
 eu.us <- df.plot$sales_total[df.plot$sphere_group=="European" & df.plot$region_sales=="USA"]
 eu.eu <- df.plot$sales_total[df.plot$sphere_group=="European" & df.plot$region_sales=="Europe"]
-eu.jp <- df.plot$sales_total[df.plot$sphere_group=="European" & df.plot$region_sales=="Japan"]
 
 ## time points
 len <- length(unique(df.plot$year))
@@ -363,12 +388,43 @@ summary(imp, "report")
 
 
 ##----------------------------------------
+## European Firms in USA Market
+##----------------------------------------
+## design matrix
+X2 <-  zoo(cbind(eu.us, us.us, eu.eu, us.eu), time.points) ## rescale by millions
+
+## run CausalImpact Bayesian Structual Time Series Inference of causal impact
+set.seed(1111)
+imp2 <- CausalImpact(X2/1000, range.pre, range.post, alpha = .01, 
+                     model.args = list(niter=10000))
+
+## create plot
+ciplot2 <- plot(imp2) + 
+  ggtitle(sprintf("European Firms in USA:  %s",pvalstr(imp2$summary$p[2]))) + 
+  ylab("US$ Bn.") +
+  theme_bw()
+## echo plot
+ciplot2
+## save plot
+ciplot2.filename <- file.path("img","causal_inference_European_pharma_rev_inUSA_99ci.png")
+ggsave(filename = ciplot2.filename, plot = ciplot2, 
+       width=5.5, height=5.5, units='in', dpi=200)
+
+## print summary and report
+summary(imp2)
+summary(imp2, "report")
+
+
+
+
+##----------------------------------------
 ## (US - EU) Firms in USA Market
-##    Controls:  Europe
 ##----------------------------------------
 ## design matrix
 X3.us <-  zoo(cbind(us.us, us.eu), time.points) ## rescale by millions
 X3.eu <-  zoo(cbind(eu.us, eu.eu), time.points) ## rescale by millions
+# d.us <- us.us-eu.us
+# X3 <-  zoo(cbind(d.us, eu.us, us.eu, eu.eu), time.points) ## rescale by millions
 
 # X3s <- X3.us / (X3.us + X3.eu) 
 X3d <- X3.us - X3.eu
@@ -384,47 +440,11 @@ ciplot3 <- plot(imp3) +
   ylab("Competitor Constellation\nRevenue Difference in USA (US$ Bn.)") +
   xlab("Year") +
   theme_bw() #+
-#annotate("text", x=as.Date("2011-11-20"), y=a.y, label="Individual\nMandate", size=4, col='darkgray') 
-## echo plot
-ciplot3
-## save plot
-ciplot3.filename <- file.path("img","causal_inference_American_pharma_rev_DIFF_inUSA_99ci.png")
-ggsave(filename = ciplot3.filename, plot = ciplot3, 
-       width=5.1, height=7, units='in', dpi=200)
-
-## print summary and report
-summary(imp3)
-summary(imp3, "report")
-
-
-
-##----------------------------------------
-## (US - EU) Firms in USA Market
-##   Controls:  Europe, Japan
-##----------------------------------------
-## design matrix
-X3.us <-  zoo(cbind(us.us, us.eu, us.jp), time.points) ## rescale by millions
-X3.eu <-  zoo(cbind(eu.us, eu.eu, eu.jp), time.points) ## rescale by millions
-
-# X3s <- X3.us / (X3.us + X3.eu) 
-X3d <- X3.us - X3.eu
-
-## run CausalImpact Bayesian Structual Time Series Inference of causal impact
-set.seed(1111)
-imp3 <- CausalImpact(X3d/1000, range.pre, range.post, alpha = .01, 
-                     model.args = list(niter=10000, prior.level.sd=1/3))
-
-## create plot
-ciplot3 <- plot(imp3) +   
-  ggtitle(sprintf("PPACA Effect in USA:  %s",pvalstr(imp3$summary$p[2]))) + 
-  ylab("Competitor Constellation\nRevenue Difference in USA (US$ Bn.)") +
-  xlab("Year") +
-  theme_bw() #+
   #annotate("text", x=as.Date("2011-11-20"), y=a.y, label="Individual\nMandate", size=4, col='darkgray') 
 ## echo plot
 ciplot3
 ## save plot
-ciplot3.filename <- file.path("img","causal_inference_American_pharma_rev_DIFF_inUSA_2ctrlEuroJap_99ci.png")
+ciplot3.filename <- file.path("img","causal_inference_American_pharma_rev_DIFF_inUSA_99ci.png")
 ggsave(filename = ciplot3.filename, plot = ciplot3, 
        width=5.1, height=7, units='in', dpi=200)
 
