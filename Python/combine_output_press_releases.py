967fc140c80b009934162ca3ed3e473c9c4fb6f9
Stephen Downing <stephendowning2008@gmail.com>
2019-05-24 03:12:52 -0500

    updates python medtrack separate and combine scripts

--- Python/combine_output_press_releases.py
+++ Python/combine_output_press_releases.py
@@ -6,6 +6,7 @@ Created on Wed May 22 00:29:51 2019
 """
 import os, pymongo, re
 import pandas as pd
+import numpy as np
 
 work_dir = 'C:\\Users\\T430\\Google Drive\\PhD\\Research\\MMC\\pharma_encounters\\mmc-pharma'
 os.chdir(os.path.join(work_dir, 'Python'))
@@ -15,46 +16,100 @@ data_dir = os.path.join(work_dir, 'medtrack_data')
 news_links_dir = os.path.join(data_dir, 'news_subcategories')
 out_dir = os.path.join(work_dir, 'medtrack_data\\COMBINED\\20190521\\press_releases')
 
+
+## FUNCTIONS
+def fix_NaN(x):
+    if isinstance(x, str) and x.lower() == 'nan':
+        return None
+    elif pd.isnull(x) and x != 0:
+        return None
+    else:
+        return x
+
+
 ## Database Credentials
 MONGO_URI = 'localhost' #'mongodb://127.0.0.1'
 MONGO_DATABASE = 'medtrack'
-MONGODB_COLLECTION  = 'news_firm'
+MONGODB_COLLECTION  = 'news_firm_separate'
 #MONGO_PORT = 27017
 MONGODB_COLLECTION_NEW  = 'press_release'
 
 ## database connection
 client = pymongo.MongoClient(MONGO_URI)
 collection = client[MONGO_DATABASE][MONGODB_COLLECTION]
-
 collection_new = client[MONGO_DATABASE][MONGODB_COLLECTION_NEW]
 
 ## LOAD DATA FROM MONGODB
 df = pd.DataFrame([ x for x in collection.find({}) ])
+
+## replace news_subcategory NaN-->None
+for col in df.columns:
+    df[col] = df[col].apply(lambda x: fix_NaN(x))
+
+##  duplicate links
+mlink = df[['_id','link']].groupby('link').filter(lambda x: len(x) > 1)
+
+## subset rows:
+## either duplicate links with is_firm_source==1, or not duplicate link
+idx_sr = (~df.link.isin(mlink.link) | ((df.is_firm_source > 0) & ~pd.isnull(df.news_subcategory)))
+idx = idx_sr[idx_sr.values == True].index
+df2 = df.iloc[idx,:].copy()
+
+###DEBUG
+#cnt2 = df2.link.value_counts()
+#z = df2.loc[df2.link == 'http://dlx.medtrack.com//UI/Shared/Lookup/LUNews.aspx?NewsID=480102',:].copy()
+#z.to_csv('TEST_DUPLICATES.csv',index_label='index')
+
+## Add pharagraph and word numbers
 
 ## COLUMN NAME MAP
 col_map = {
     '_id': 'ID',
     'release_date': 'DATE',
     'news_headline': 'HEADLINE',
-    'article': 'FULLTEXT',
-    'company_name': 'CONAME',
+    #'article': 'FULLTEXT',
+    'article_text': 'BODYTEXT',
+    'about_firm_1': 'ABOUTFIRM1',
+    'about_firm_2': 'ABOUTFIRM2',
+    'about_firm_3': 'ABOUTFIRM3',
+    'about_firm_4': 'ABOUTFIRM4',
+    'about_firm_5': 'ABOUTFIRM5',
+    'about_firm_6': 'ABOUTFIRM6',
+    'source': 'SRC',
+    'is_firm_source': 'ISFIRMSRC',
     'firm': 'FIRMID',
-    'development_phase': 'DEVPHASE',
+    'company_name': 'CONAME',
     'geography': 'COUNTRY',
     'indication': 'INDICATION',
     'news_category': 'CATEGORY',
     'news_subcategory': 'SUBCAT',
     'product_name': 'PRODNAME',
+    'development_phase': 'DEVPHASE',
     'link': 'LINK',
+    'source_2': 'SRC2',
+    'source_3': 'SRC3',
+    'source_4': 'SRC4',
+    'about_other_1': 'ABOUTOTR1',
+    'about_other_2': 'ABOUTOTR2',
+    'about_other_3': 'ABOUTOTR3',
+    'about_other_4': 'ABOUTOTR4',
+    'about_other_5': 'ABOUTOTR5',
+    'about_other_6': 'ABOUTOTR6',
+    'about_other_7': 'ABOUTOTR7',
+    'about_other_8': 'ABOUTOTR8',
+    'about_other_9': 'ABOUTOTR9',
+    'about_other_10': 'ABOUTOTR10',
+    'about_other_11': 'ABOUTOTR11',
+    'about_other_12': 'ABOUTOTR12'
 }
 
 ## CREATE NEW DATAFRAME SELECTING COLUMNS
 col_select = [key for key in col_map.keys()]
-df2 = df[col_select].copy()
+df3 = df2[col_select].copy()
 
 ## MAP NAMES
-df2.columns = [col_map[key] for key in col_map.keys()]
+df3.columns = [col_map[key] for key in col_map.keys()]
 
 ## CREATE NEW MONGODB COLLECTION
-collection_new.insert_many([row.to_dict() for i,row in df2.iterrows()])
+collection_new.insert_many([row.to_dict() for i,row in df3.iterrows()])
 
--- Python/run_medtrack_article_separate.py
+++ Python/run_medtrack_article_separate.py
@@ -4,12 +4,12 @@ Created on Thu Apr 18 01:21:28 2019
 
 @author: T430
 """
-import os, pymongo
+import os, pymongo, re
 import pandas as pd
+from bson.objectid import ObjectId
 ## if called from outside project dir
 work_dir = 'C:\\Users\\T430\\Google Drive\\PhD\\Research\\MMC\\pharma_encounters\\mmc-pharma'
 os.chdir(os.path.join(work_dir, 'Python'))
-from medtrack_article_parser import MedtrackArticle, get_user_agent
 
 ## directories 
 data_dir = os.path.join(work_dir, 'medtrack_data')
@@ -22,31 +22,124 @@ MONGODB_COLLECTION  = 'news_firm'
 MONGODB_COLLECTION_NEW  = 'news_firm_separate'
 #MONGO_PORT = 27017
 
+## map Firm ID to firm name search pattern
+firms = {
+    'pfizer': ['pfizer'],
+    'gsk': ['glaxo'],
+    'merck': ['merck','plough'],  ## 2009 merck merged with schering-plough
+    'jnj': ['johnson'],
+    'novartis': ['novartis'],
+    'roche': ['roche'],
+    'sanofi': ['sanofi'],
+    'astrazeneca': ['astrazeneca'],
+    'bms': ['bristol','squibb'],
+    'abbott': ['abbott'],
+    'teva': ['teva'],
+    'bayer': ['bayer'],
+    'lilly': ['lilly'],
+    'novonordisk': ['nordisk'],
+    'gilead': ['gilead'],
+    'amgen': ['amgen'],
+    'genzyme': ['genzyme'],
+    'biogen': ['biogen'],
+    'abbvie': ['abbvie'],
+    'mylan': ['mylan'],
+}
 
 def run():
     """ Main run script
     """
+    print('processing article separation')
     ## database connection
     client = pymongo.MongoClient(MONGO_URI)
     collection = client[MONGO_DATABASE][MONGODB_COLLECTION]
     collection_new = client[MONGO_DATABASE][MONGODB_COLLECTION_NEW]
-
     
     ## read in collection contents if any
     df = pd.DataFrame([ x for x in collection.find({}) ])
     
+    ## Process all rows to new collection
     for index, row in df.iterrows():
-        if index % 100 == 0:
+        
+        if index % 500 == 0:
             print(' %s' % index)
         #print(type(row.article_strip))
-        if 'article_strip' in row.index and isinstance(row.article_strip, str) and len(row.article_strip):
+        if 'article_source' in row.index and isinstance(row.article_source, str) and len(row.article_source):
             continue
-        article = MedtrackArticle(row.link, collection, row.to_dict(), USER_AGENT)
-        article.parse()
-        article.update_by_id({
-            'article': article.item['article'],
-            'article_strip': row.article
-        })
+        
+        ## press release record as a dictionary
+        item = row.to_dict()
+        
+        ## fix parsed null types that cannot save in MongoDB (pd.NaT, ...)
+        for key in item.keys():
+            if pd.isnull(item[key]):
+                item[key] = None
+        
+        ## Patch missing firm IDs (if multiple Company Name listed)
+        firm_set = set()
+        for firm_id in firms.keys():
+            for firm_pattern in firms[firm_id]:
+                if firm_pattern in item['company_name'].lower():
+                    firm_set.update([firm_id])
+        item['firm'] = '|'.join(firm_set)
+        
+        ## Source
+        source_pattern = '\\r\\n\\r\\nSource:\s.*(?!\\r|\\n)'
+        sources = re.findall(source_pattern, item['article'])
+        is_firm_source = 0
+        for i, source_i in enumerate(sources):
+            source_i_str = re.sub('\\.$','', source_i.replace('\r\n\r\nSource:', '') ).strip()  ## remove final period and strip whitespace
+            if i == 0:
+                key = 'source'
+                for firm_id in item['firm'].split('|'):
+                    if firm_id in firms.keys():
+                        for firm_pattern in firms[firm_id]:
+                            if firm_pattern in source_i_str.lower():
+                                is_firm_source = 1
+                item['is_firm_source'] = is_firm_source
+            else:
+                key = 'source_%s' % (i+1)
+            item[key] = source_i_str 
+        
+        ## About sections
+        item['about_firm_1'] = None ## set default firm about in case not in press release
+        about_pattern = '\\r\\n\\r\\nAbout.*\\r\\n\\r\\n.*(?!\\r|\\n)'
+        abouts = re.findall(about_pattern, item['article'])
+        about_firm_cnt = 0
+        about_other_cnt = 0
+        for i, about_i in enumerate(abouts):
+            about_i_text = re.sub('\\r\\n\\r\\nAbout.*\\r\\n\\r\\n','', about_i)
+            about_i_firm = re.sub('(\\r\\n\\r\\nAbout|\\r\\n\\r\\n)','',about_i.replace(about_i_text,'')).strip()
+            ##--------------
+            is_focal_firm = False
+            for firm_id in item['firm'].split('|'):
+                if firm_id in firms.keys():
+                    for firm_pattern in firms[firm_id]:
+                        if firm_pattern in about_i_firm.lower():
+                            is_focal_firm = True
+            ##--------------
+            if is_focal_firm:
+                key = 'about_firm_%s' % (about_firm_cnt + 1)
+                about_firm_cnt += 1
+            else:
+                key = 'about_other_%s' % (about_other_cnt + 1)
+                about_other_cnt += 1
+            item[key] = '|'.join([about_i_firm,about_i_text])
+        
+        ## Article without the Abouts and Sources
+        ending_pattern = '(\\r\\n\\r\\nAbout.*\\r\\n\\r\\n.*(?!\\r|\\n)|\\r\\n\\r\\nSource:\s.*(?!\\r|\\n))'
+        article_text = re.sub(ending_pattern,'', item['article'])
+        item['article_text'] = article_text.strip()
+
+        ## Save to new collection
+        if not collection_new.find_one({'_id':ObjectId(item['_id'])}):
+            _id = collection_new.insert_one(item)
+            if not _id or not _id.inserted_id:
+                print(' failed to insert',item['_id'])
+        
+        # ## ****DEBUG***
+        # if index > 200:
+        #     break
 
 
 if __name__ == '__main__':
